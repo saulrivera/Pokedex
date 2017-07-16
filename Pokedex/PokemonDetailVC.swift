940fa3dc1656512c9888e83e3d64454bb5ac59dd
Saúl Rivera <saulriveal@gmail.com>
2017-07-16 11:31:20 -0500

    Added next segue and Iboutlets for detail descriptions

--- Pokedex/PokemonDetailVC.swift
+++ Pokedex/PokemonDetailVC.swift
@@ -11,6 +11,17 @@ import UIKit
 class PokemonDetailVC: UIViewController {
     
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descriptionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var defenseLbl: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexLbl: UILabel!
+    @IBOutlet weak var weightLbl: UILabel!
+    @IBOutlet weak var attackLbl: UILabel!
+    @IBOutlet weak var evoLbl: UILabel!
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImh: UIImageView!
     
     var pokemon: Pokemon!
 
