5edff5a1af5cf37e9290dfcabe2f5ba79f50d79b
Saúl Rivera <saulriveal@gmail.com>
2017-07-15 22:50:12 -0500

    Added Collection View

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Saul Rivera on 7/15/17.
+//  Copyright © 2017 Dymtech. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+    
+    
+    
+    
+    
+}
